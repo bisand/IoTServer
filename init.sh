d328c3cacf7968385f3b29fb604e6f32e1b714b0
André Biseth <andre@biseth.net>
2019-02-19 14:58:37 +0000

    Made some config changes.

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 mkdir -p /srv/docker/influxdb/data
 mkdir -p /srv/docker/grafana/data
 mkdir -p /root/data/config
+mkdir -p /root/data/config/grafana
 mkdir -p /root/data/letsencrypt
 mkdir -p /root/data/certs
 chown 472:472 /srv/docker/grafana/data
+touch /root/data/config/grafana/custom.ini
