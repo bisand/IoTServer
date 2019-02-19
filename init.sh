5e71dc79ce964bec86ba1d019e92d5c32c89e207
André Biseth <andre@biseth.net>
2019-02-19 10:03:01 +0000

    Added init script

---
+++ init.sh
@@ -0,0 +1,6 @@
+mkdir -p /srv/docker/influxdb/data
+mkdir -p /srv/docker/grafana/data
+mkdir -p /root/data/config
+mkdir -p /root/data/letsencrypt
+mkdir -p /root/data/certs
+chown 472:472 /srv/docker/grafana/data
